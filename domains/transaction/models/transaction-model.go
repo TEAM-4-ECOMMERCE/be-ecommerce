d1adae03012dbc27009a3548b8adc446fa608631
arch-havis <havisiqbalsyah@gmail.com>
2022-09-15 14:18:41 +0700

    feature transaksi: update model

--- domains/transaction/models/transaction-model.go
+++ domains/transaction/models/transaction-model.go
@@ -21,6 +21,8 @@ type TransactionDetail struct {
 	ProductID     uint
 	Qty           uint
 	Subtotal      float64
+
+	Product Product
 }
 
 type Address struct {
@@ -38,4 +40,39 @@ type CreditCard struct {
 	Number string
 	CVV    string
 	Date   float64
+}
+
+type Cart struct {
+	gorm.Model
+	ProductID uint
+	UserID    uint
+	Qty       uint
+	Subtotal  float64
+	User      User    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Product   Product `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string
+	Password string
+}
+
+type Product struct {
+	gorm.Model
+	CategoryID  uint
+	UserID      uint
+	Name        string
+	Price       float64
+	Stock       uint
+	Description string
+	ImageUrl    string
+	Category    Category `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	User        User     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+type Category struct {
+	gorm.Model
+	Name string
 }
