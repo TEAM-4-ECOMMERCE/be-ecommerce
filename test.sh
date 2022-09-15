0b936b909912634b545631d189b5a977edd23d1d
arch-havis <havisiqbalsyah@gmail.com>
2022-09-15 09:03:38 +0700

    feature category: fix some response

--- test.sh
+++ test.sh
@@ -4,7 +4,4 @@ go test ./domains/auth/... -coverprofile=cover.out && go tool cover -html=cover.
 go test ./domains/register/... -coverprofile=cover.out && go tool cover -html=cover.out
 go test ./domains/product/... -coverprofile=cover.out && go tool cover -html=cover.out
 go test ./domains/cart/... -coverprofile=cover.out && go tool cover -html=cover.out
-<<<<<<< HEAD
 go test ./domains/category/... -coverprofile=cover.out && go tool cover -html=cover.out
-=======
->>>>>>> 9c7ae9202acf126cf3619b11f106b5f1c483be8e
