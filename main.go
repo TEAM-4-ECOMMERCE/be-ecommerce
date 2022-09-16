ec40f09889c90ef72a28f7404b87b7802cf66ad9
arch-havis <havisiqbalsyah@gmail.com>
2022-09-16 13:37:59 +0700

    cors add OPTION method

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
-		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
 	}))
 
 	routes.InitRoutes(e, db, cfg)
